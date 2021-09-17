d7d6d7df840a89482491eac54aaee56687e827fd
Jeffrey Teh <59184460+Jeffderp22@users.noreply.github.com>
2021-09-17 13:56:31 -0700

    Palindrome 2

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,26 +1,41 @@
-#Include <iostream>
+#include <iostream>
 #include <cstring>
 
 using namespace std;
 
 int main()
 {
-  char str[81];
-  char input[81];
+  char str[80];
+  char input[80];
   cout << "Please enter a string!" << endl;
-  cin >> input;
+  cin >> input >> endl;
   int count = 0;
-  for(int i = 0; i<80, i++){
-    if (isalpha(input[i]) || isdigit(input[i]){
+  for(int i = 0; i<80, i++;)
+    {
+    if(ispunct(input[i]) == 0)
+      {
+	 remove(begin(str), end(str), input[i]);
+      }
+    if (isalpha(input[i]) || isdigit(input[i]))
+     {
     str[count] = input[i];
-      count++
+    count++;
      }
       else
 	{
 	  cout << "Please enter only numbers or letters." << endl;
 	}
-      }
-  reverse(str.begin(), str.end());
-    return 0;
+    }
+    //https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/
+  reverse(begin(str), end(str));
+  if(strcmp(str, input) == 0)
+    {
+      cout << "Palindrome" << endl;
+    }
+  else
+    {
+      cout << "Not a Palindrome" << endl;  
+    }
+    input[count] = '\0';
+  return 0;
   }
-
