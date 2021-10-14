58db7ec29af150d9f5a1a3aa41bb44399d2ad6df
Jeffrey Teh <59184460+Jeffderp22@users.noreply.github.com>
2021-10-14 13:32:33 -0700

    Update Palindrome.cpp
    Cleared memory before reading input, and fixed skipping over certain characters.

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -12,6 +12,13 @@
     char reverse[80];
     char input[80];
     char str[80];
+    memset(input, 0, 80);
+	  memset(reverse, 0, 80);
+    memset(str, 0, 80);
+	for(int i=0; i < 80; i++){
+  str[i] = '\0';
+    }
+    
     cout << "Please enter a string!" << endl;
     cin.get(input, 80 , '\n');
     cin.get();
@@ -26,7 +33,7 @@
 	}
 	else
 	a--;
-	i++; a++;
+	a++;
     }
     int length2 = strlen(str);
     int j = 0;
@@ -39,7 +46,6 @@
 	str[length2] = '\0';
     //Checking to see if it's a Palindrome
          if(strcmp(reverse, str) == 0){
-        
           cout << "Palindrome" << endl;
           }
         else
